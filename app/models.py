1249b6e068138c414eb3d325bb5b59de590fd224
DzrJob <dzr_job@163.com>
2019-01-07 20:29:25 +0800

    tag 8a

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from . import db
+# 模型中加入密码散列 注册用户 验证用户
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # 定义模型
 class Role(db.Model):
@@ -21,8 +23,27 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    # hash密码
+    password_hash = db.Column(db.String(128))
+
     # 定义外键role_id 关联Role的主键roles.id
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    # getter密码不可读
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    # setter密码设置密码 加密方法和盐值默认
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    # 散列密码与输入密码匹配验证
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+
